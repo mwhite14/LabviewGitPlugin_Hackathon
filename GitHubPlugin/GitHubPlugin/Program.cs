2868d0c6c8aaefe803e46c588a58cb6198cabe16
Colton Tyler Hurt <coltonhurt@live.com>
2014-07-16 16:49:09 -0500

    Added in changes for github plugin stuff

--- GitHubPlugin/GitHubPlugin/Program.cs
+++ GitHubPlugin/GitHubPlugin/Program.cs
@@ -8,9 +8,15 @@ namespace GitHubPlugin
 {
     class Program
     {
+        private static string localRepo = "C:/Users/colto_000/Documents/GitHubPluginProj/Proj";
+
         static void Main(string[] args)
         {
-           
+            GitPull pull = new GitPull(localRepo);
+            string test = pull.PullChanges();
+
+            Console.Write(test);
+            Console.ReadKey();
         }
     }
 }
