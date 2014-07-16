770f0025d24c92f29543e968fe55b4fea5ba3160
Colton Tyler Hurt <coltonhurt@live.com>
2014-07-16 16:06:40 -0500

    Fixed push, needs refining.

--- GitHubPlugin/GitHubPlugin/GitPush.cs
+++ GitHubPlugin/GitHubPlugin/GitPush.cs
@@ -16,7 +16,7 @@ namespace GitHubPlugin
         public GitPush(string localRepoPath) 
         { 
             repo = new Repository(localRepoPath);
-            remote = repo.Network.Remotes["orgin"];
+            remote = repo.Network.Remotes["origin"];
         }
 
         /// <summary>
@@ -25,7 +25,14 @@ namespace GitHubPlugin
         /// <returns>string if error, else null</returns>
         public string push()
         {
-            try { repo.Network.Push(remote, "HEAD", "refs/heads/orgin/"); }
+            PushOptions pushOps = new PushOptions();
+            UsernamePasswordCredentials creds = new UsernamePasswordCredentials();
+            creds.Username = "";
+            creds.Password = "";
+            pushOps.Credentials = creds;
+
+
+            try { repo.Network.Push(repo.Network.Remotes["origin"], "HEAD", "refs/heads/master", pushOps, new Signature("Colton", "That.that.net", DateTime.Now), "This is a test"); }
             catch (Exception e) { return "Message: " + e.Message + " Cause: " + e.InnerException + " Error#: " + e.HResult; }
             return null;
         }
