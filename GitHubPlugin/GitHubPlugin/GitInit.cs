72e689e9233224397d77c86fa2dd8ab5df44e052
Colton Tyler Hurt <coltonhurt@live.com>
2014-07-16 13:34:03 -0500

    Added GitInit and fixes by adding in NativeBinaries folder

---
+++ GitHubPlugin/GitHubPlugin/GitInit.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibGit2Sharp;
+
+namespace GitHubPlugin
+{
+    class GitInit
+    {
+        private string _relativeFilePath;
+
+        public string RelativeFilePath { get { return _relativeFilePath; } }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public GitInit()
+        { }
+
+        public GitInit(string relativePath)
+        {
+            _relativeFilePath = relativePath;
+        }
+
+        public void setRelativeFilePath(string newPath)
+        {
+            _relativeFilePath = newPath;
+        }
+
+        /// <summary>
+        /// Initializes the repository with the given relative path name
+        /// </summary>
+        /// <returns></returns>
+        public bool InitializeRepo()
+        {
+            //If the string is empty
+            //Need to ensure that the path is a relative path, can be done later
+            if (_relativeFilePath.Length < 1)
+                return false;
+
+            //Initi the repo
+            string rootPath = Repository.Init(_relativeFilePath);
+
+            if (rootPath.Contains(".git"))
+                return true;
+            else
+                return false;
+        }
+
+        
+    }
+}
